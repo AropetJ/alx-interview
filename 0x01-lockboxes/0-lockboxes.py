c7d64dbc68016a1fe467bac18080b2eb2622300b
AropetJ <aropetjoel32@gmail.com>
2024-01-09 12:03:05 +0100

    Added a space at the end of the readme and removed spaces from the lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-# 0-lockboxes.py
+'''0-lockboxes.py
+'''
 
 
 def canUnlockAll(boxes):
@@ -12,11 +13,9 @@ def canUnlockAll(boxes):
     unlocked_boxes = [False] * num_boxes
     unlocked_boxes[0] = True
     unused_keys = set(boxes[0])
-
     while unused_keys:
         key = unused_keys.pop()
         if key < num_boxes and not unlocked_boxes[key]:
             unlocked_boxes[key] = True
             unused_keys.update(boxes[key])
-
     return all(unlocked_boxes)
