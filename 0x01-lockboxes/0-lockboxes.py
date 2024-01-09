18f9e83353d1288e1525638e3722fde1fe8b53a0
AropetJ <aropetjoel32@gmail.com>
2024-01-09 12:05:10 +0100

    Tested with a deque collection

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 '''0-lockboxes.py
 '''
+from collections import deque
 
 
 def canUnlockAll(boxes):
     """A method that determines if all the boxes can be opened
-
     Args:
         boxes (list of lists): Contains the boxes to be opened
     """
     num_boxes = len(boxes)
     unlocked_boxes = [False] * num_boxes
     unlocked_boxes[0] = True
-    unused_keys = set(boxes[0])
+    unused_keys = deque(boxes[0])
     while unused_keys:
         key = unused_keys.pop()
         if key < num_boxes and not unlocked_boxes[key]:
             unlocked_boxes[key] = True
-            unused_keys.update(boxes[key])
+            unused_keys.extend(boxes[key])
     return all(unlocked_boxes)
