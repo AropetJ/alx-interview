cb7e9bd6520317150560cb3303104381e20894e3
AropetJ <aropetjoel32@gmail.com>
2024-01-18 15:15:26 +0100

    Attempted task 1

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,8 +4,20 @@
 
 
 def minOperations(n):
-    '''minOperations
-    '''
+    """
+    Calculates the minimum number of operations required
+    to reach a given number 'n' using only the operations
+    of doubling the current number or adding 1 to it.
+    Args:
+        n (int): The target number.
+    Returns:
+        int: The minimum number of operations required.
+    Examples:
+        >>> minOperations(6)
+        5
+        >>> minOperations(9)
+        6
+    """
     if n <= 1:
         return 0
     i = 2
