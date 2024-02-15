9b2a6b96be4893898e61f30a9d571ecdc7ae83b1
AropetJ <aropetjoel32@gmail.com>
2024-02-15 12:38:40 +0100

    Adjusted code to fit linter

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,30 +1,32 @@
 #!/usr/bin/node
 const request = require('request')
-const API_URL = 'https://swapi-api.hbtn.io/api'
+const movieId = process.argv[2]
+const API_URL = `https://swapi.dev/api/films/${movieId}/`
 
-if (process.argv.length > 2) {
-  const movieId = process.argv[2]
-  request(`${API_URL}/films/${movieId}/`, (err, _, body) => {
-    if (err) {
-      console.error(err)
-      return
-    }
-    const movie = JSON.parse(body)
-    const characterURL = movie.characters
-    const getCharacterNamePromises = characterURL.map(characterURL => {
-      return new Promise((resolve, reject) => {
-        request(characterURL, (error, _, characterBody) => {
-          if (error) {
-            reject(error)
-            return
-          }
-          const character = JSON.parse(characterBody)
-          resolve(character.name)
-        })
+request(API_URL, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error)
+    return
+  }
+  if (response.statusCode !== 200) {
+    console.error('Invalid status code:', response.statusCode)
+    return
+  }
+  const film = JSON.parse(body)
+  const charactersURL = film.characters
+  const getCharacterNamesPromises = charactersURL.map(characterURL => {
+    return new Promise((resolve, reject) => {
+      request(characterURL, (error, _, characterBody) => {
+        if (error) {
+          reject(error)
+          return
+        }
+        const character = JSON.parse(characterBody)
+        resolve(character.name)
       })
     })
-    Promise.all(getCharacterNamePromises)
-      .then(characterNames => console.log(characterNames.join('\n')))
-      .catch(error => console.error(error))
   })
-}
+  Promise.all(getCharacterNamesPromises)
+    .then(characterNames => console.log(characterNames.join('\n')))
+    .catch(error => console.error(error))
+})
