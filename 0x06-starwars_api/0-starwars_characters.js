136ad8337e2083a82c6e88c466b9bc74a2980aa5
AropetJ <aropetjoel32@gmail.com>
2024-02-15 12:31:02 +0100

    Adjusted code to fit linter

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,34 +1,34 @@
 #!/usr/bin/node
-const request = require('request');
+const request = require('request')
 
-const API_URL = 'https://swapi-api.hbtn.io/api';
+const API_URL = 'https://swapi-api.hbtn.io/api'
 
 if (process.argv.length > 2) {
-    const movieId = process.argv[2];
-    request(`${API_URL}/films/${movieId}/`, (err, _, body) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
+  const movieId = process.argv[2]
+  request(`${API_URL}/films/${movieId}/`, (err, _, body) => {
+    if (err) {
+      console.error(err)
+      return
+    }
 
-        const movie = JSON.parse(body);
-        const characterURL = movie.characters;
+    const movie = JSON.parse(body)
+    const characterURL = movie.characters
 
-        const getCharacterNamePromises = characterURL.map(characterURL => {
-            return new Promise((resolve, reject) => {
-                request(characterURL, (error, _, characterBody) => {
-                    if (error) {
-                        reject(error);
-                        return;
-                    }
-                    const character = JSON.parse(characterBody);
-                    resolve(character.name);
-                });
-            });
-        });
+    const getCharacterNamePromises = characterURL.map(characterURL => {
+      return new Promise((resolve, reject) => {
+        request(characterURL, (error, _, characterBody) => {
+          if (error) {
+            reject(error)
+            return
+          }
+          const character = JSON.parse(characterBody)
+          resolve(character.name)
+        })
+      })
+    })
 
-        Promise.all(getCharacterNamePromises)
-            .then(characterNames => console.log(characterNames.join('\n')))
-            .catch(error => console.error(error));
-    });
+    Promise.all(getCharacterNamePromises)
+      .then(characterNames => console.log(characterNames.join('\n')))
+      .catch(error => console.error(error))
+  })
 }
