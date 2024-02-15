f31a8f33b6bbded9ab6440f673351a9c0d290386
AropetJ <aropetjoel32@gmail.com>
2024-02-15 12:41:32 +0100

    Adjusted code to fit linter

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,29 +4,16 @@ const movieId = process.argv[2]
 const API_URL = `https://swapi.dev/api/films/${movieId}/`
 
 request(API_URL, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error)
+  if (error || response.statusCode !== 200) {
+    console.error('Error:', error || `Invalid status code: ${response.statusCode}`)
     return
   }
-  if (response.statusCode !== 200) {
-    console.error('Invalid status code:', response.statusCode)
-    return
-  }
-  const film = JSON.parse(body)
-  const charactersURL = film.characters
-  const getCharacterNamesPromises = charactersURL.map(characterURL => {
-    return new Promise((resolve, reject) => {
-      request(characterURL, (error, _, characterBody) => {
-        if (error) {
-          reject(error)
-          return
-        }
-        const character = JSON.parse(characterBody)
-        resolve(character.name)
-      })
-    })
-  })
-  Promise.all(getCharacterNamesPromises)
-    .then(characterNames => console.log(characterNames.join('\n')))
+  const charactersURL = JSON.parse(body).characters
+  Promise.all(charactersURL.map(url => new Promise((resolve, reject) =>
+    request(url, (error, _, body) =>
+      error ? reject(error) : resolve(JSON.parse(body).name)
+    )
+  )))
+    .then(names => console.log(names.join('\n')))
     .catch(error => console.error(error))
 })
