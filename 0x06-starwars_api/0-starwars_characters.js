9a3fc4b64f217d9bb0b5e2c2150762f3ec87584d
AropetJ <aropetjoel32@gmail.com>
2024-02-15 12:34:48 +0100

    Adjusted code to fit linter

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 const request = require('request')
-
 const API_URL = 'https://swapi-api.hbtn.io/api'
 
 if (process.argv.length > 2) {
@@ -10,10 +9,8 @@ if (process.argv.length > 2) {
       console.error(err)
       return
     }
-
     const movie = JSON.parse(body)
     const characterURL = movie.characters
-
     const getCharacterNamePromises = characterURL.map(characterURL => {
       return new Promise((resolve, reject) => {
         request(characterURL, (error, _, characterBody) => {
@@ -26,7 +23,6 @@ if (process.argv.length > 2) {
         })
       })
     })
-
     Promise.all(getCharacterNamePromises)
       .then(characterNames => console.log(characterNames.join('\n')))
       .catch(error => console.error(error))
