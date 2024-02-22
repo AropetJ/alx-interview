dd73f8b1652e9527088d661d3e7898c1bef3bb8a
AropetJ <aropetjoel32@gmail.com>
2024-02-22 21:17:08 +0100

    Attempted the task - 7

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-"""
-0-ratate_2d_matrix.py - a function that rotates a 2d matrix
+"""0-ratate_2d_matrix.py
 """
 
 
 def rotate_2d_matrix(matrix):
-    """
-    Rotates a 2D matrix 90 degrees clockwise
-    Args:
-        matrix (List): 2D matrix to be rotated
+    """Rotates a 2D matrix 90 degrees clockwise
     """
     if len(matrix) <= 0:
         return
@@ -16,7 +12,6 @@ def rotate_2d_matrix(matrix):
         return
     if not all(isinstance(row, list) for row in matrix):
         return
-
     for i in range(len(matrix)):
         for j in range(i + 1, len(matrix)):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
